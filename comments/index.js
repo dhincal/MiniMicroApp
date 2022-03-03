7e7570e397a98c5b5a0b0209b6314f3d072e3ae7
Deniz <denizhincal05@gmail.com>
2022-03-03 12:03:25 +0300

    Comment Service Implement
    Comment service is now working fine and ready to use

--- comments/index.js
+++ comments/index.js
@@ -3,21 +3,30 @@ const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 
 const app = express();
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req, res) =>{
+app.get('/posts/:id/comments', (req, res) => {
+
+    res.send(commentsByPostId[req.params.id]);
 
 });
 
-app.post('/posts/:id/comments', (req, res) =>{
+app.post('/posts/:id/comments', (req, res) => {
+
     const commentId = randomBytes(4).toString('hex');
-    const {content} = req.body;
-    
+    const { content } = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({id: commentId, content});
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
 
 });
 
-app.listen(4001, () =>{
-    console.log("Listening in 4001")
-})
+app.listen(4001, () => {
+    console.log('listening on 4001');
+});
