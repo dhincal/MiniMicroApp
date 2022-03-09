d580f5b3d01dde27471347a11f6ac98808e85819
Deniz <denizhincal05@gmail.com>
2022-03-09 11:08:56 +0300

    Add Visual Elemets For Comment Moderation
    Now with the moderation service some comments that includes bad words will permitted and won't show on the screen besides the only thing they will see is "comment rejected". A bug in event-bus service has been fixed in this update.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,6 +2,21 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
+
+    switch (comment.status) {
+      case 'approved':
+        return <li key={comment.id}>{comment.content}</li>;
+        break;
+      
+      case 'rejected':
+        return <li key={comment.id}><i className="text-danger">this comment is rejected</i></li>;  
+        break;
+      
+      case 'pending':
+        return <li key={comment.id}><i className="text-warning">this comment is waiting to be approved</i></li>;
+        break;
+    }
+
     return <li key={comment.id}>{comment.content}</li>;
   });
 
