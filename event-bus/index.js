aef07bcd8495f16d514a87cf4216658a6aa4c934
Deniz <denizhincal05@gmail.com>
2022-03-15 16:27:22 +0300

    Update Service Adress Of EventBus And Post Services
    Now these two Services are in communication with no issue

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
-//a
+
 const events = [];
 
 app.post("/events", (req, res) => {
@@ -12,11 +12,11 @@ app.post("/events", (req, res) => {
 
     events.push(event);
 
-    axios.post("http://localhost:4000/events", event).catch((err) => {
+    axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
         console.log(err.message);
     });
 
-    axios.post("http://localhost:4001/events", event).catch((err) => {
+    /*axios.post("http://localhost:4001/events", event).catch((err) => {
         console.log(err.message);
     });
 
@@ -26,7 +26,7 @@ app.post("/events", (req, res) => {
 
     axios.post("http://localhost:4003/events", event).catch((err) => {
         console.log(err.message);
-    });
+    });*/
 
     res.send({ status: "OK" });
 });
